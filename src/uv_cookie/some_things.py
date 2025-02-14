f81d1529a9b02608c3b47ac22c1a825067816840
Logan Bennett <bennett.j.logan@gmail.com>
2025-02-13 20:15:19 -0800

    add tests, actions, and pre-commit

---
+++ src/uv_cookie/some_things.py
@@ -0,0 +1,19 @@
+print(
+    'kkkkkkkkjlskdjflksdjflsdjflkds jflksdjflkjdslkfjsd lkjflsdjflsdjfldsjflsdkjflsdjflksdjflksdjflksdjflskdjflsdkjfklsdjkl'
+)
+
+
+def test_func(x):
+    value = x + 1
+    return value
+
+
+def return_boo():
+    return 'boo'
+
+
+a = 1
+
+junk = '22'
+
+hello = 'fast'
---
+++ tests/some_things_test.py
@@ -0,0 +1,5 @@
+from uv_cookie.some_things import return_boo
+
+
+def test_return_boo():
+    assert return_boo() == 'boo'
