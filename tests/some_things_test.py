1af10f9cbcc20ef0c46082b709ae4ca6d446aa56
Logan Bennett <bennett.j.logan@gmail.com>
2025-02-13 21:33:38 -0800

    restructure

--- tests/some_things_test.py
+++
@@ -1,5 +0,0 @@
-from uv_cookie.some_things import return_boo
-
-
-def test_return_boo():
-    assert return_boo() == 'boo'
---
+++ {{cookiecutter.project_slug}}/tests/{{cookiecutter.file_name}}_test.py
@@ -0,0 +1,5 @@
+from {{cookiecutter.project_slug}}.{{cookiecutter.file_name}} import return_boo
+
+
+def test_return_boo():
+    assert return_boo() == 'boo'
